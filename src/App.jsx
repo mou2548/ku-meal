6ffbc35e2780ff1fd1619dd6d97996259143a43b
Piyaphat <piyaphat20104@gmail.com>
2025-06-14 23:01:18 +0700

    'debugApp.jsx'

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-// import { supabase } from '../utils/supabase'
+import { supabase } from '../utils/supabase'
 import './App.css'
 
 import { Routes, Route } from 'react-router-dom'
@@ -7,22 +7,24 @@ import Login from './pages/Login'
 import Menu from './pages/Menu'
 import Canteens from './pages/Canteens'
 import Qr from './pages/QRCode'
-import Queue from './pages/Queue'
-import Shop from './pages/Shops'
+import QueueScreen from './pages/Queue'
+// import Shop from './pages/Shops'
+import PayScreen from './pages/PayScreen'
 
 
 function App() {
   return (
-    <Router>
+    <>
       <Routes>
-        <Route path="/" element={<PayScreen />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/payscreen" element={<PayScreen />} />
         <Route path="/queue/:queueNumber" element={<QueueScreen />} />
         <Route path="/canteens" element={<Canteens />} />
         <Route path="/Qrcode" element={<Qr />} />
-        <Route path="/queue" element={<Queue />} />
-        <Route path="/shop" element={<Shop />} />
+        {/* <Route path="/shop" element={<Shop />} /> */}
       </Routes>
-    </Router>
+    </>
   );
 }
 
