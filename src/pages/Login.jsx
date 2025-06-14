f73d6e1ff3508a4f9fa826b823ac6ba9190b0b0e
Rantod-cmd <128260878+Rantod-cmd@users.noreply.github.com>
2025-06-14 18:17:14 +0700

    หน้า login รวมไฟล์

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,25 @@
 import React, { useState } from "react";
 
-export default function Login({ onSuccess }) {
+export default function LoginWrapper() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <div className="h-screen w-screen bg-[#FBF6E9] flex flex-col overflow-hidden">
+      <div className="bg-green-700 h-[6vh] w-full"></div>
+      <div className="bg-[#74c09b] px-12 py-6 flex items-center justify-center space-x-6 w-full">
+        <div className="bg-green-700 w-16 h-16 rounded-full flex items-center justify-center">
+          <span className="text-white text-3xl">🍴</span>
+        </div>
+        <h1 className="text-5xl font-bold text-gray-800 ">KUMeal</h1>
+      </div>
+      <div className="flex-1 flex items-center justify-center w-full">
+        <Login onSuccess={() => setLoggedIn(true)} />
+      </div>
+    </div>
+  );
+}
+
+function Login({ onSuccess }) {
   const [account, setAccount] = useState("");
   const [password, setPassword] = useState("");
 
