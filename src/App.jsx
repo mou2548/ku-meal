bf8673f3598d0ca0d5bf11bcc5f379b2a884b54d
Patiph4t <134424515+mou2548@users.noreply.github.com>
2025-06-14 23:04:15 +0700

    Preparing for merging

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 import { useEffect, useState } from 'react'
-// import { supabase } from '../utils/supabase'
+import { supabase } from '../utils/supabase'
 import './App.css'
 
 import { Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Menu from './pages/Menu'
-import Canteens from './pages/Canteens'
-import Qr from './pages/QRCode'
-import Queue from './pages/Queue'
-import Shop from './pages/Shops'
+// import Login from './pages/Login'
+// import Menu from './pages/Menu'
+// import Canteens from './pages/Canteens'
+// import QueueScreen from './pages/Queue'
+import PayScreen from './pages/PayScreen'
+// import Qr from './pages/QRCode'
+// import Shop from './pages/Shops'
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PayScreen />} />
-        <Route path="/queue/:queueNumber" element={<QueueScreen />} />
-        <Route path="/canteens" element={<Canteens />} />
-        <Route path="/Qrcode" element={<Qr />} />
-        <Route path="/queue" element={<Queue />} />
-        <Route path="/shop" element={<Shop />} />
-      </Routes>
-    </Router>
+    <>
+      <h1>YO</h1>
+        <Routes>
+          {/* <Route path="/" element={<Login />} /> */}
+          <Route path="/payscreen" element={<PayScreen />} />
+          {/* <Route path="/queue/:queueNumber" element={<QueueScreen />} /> */}
+          {/* <Route path="/canteens" element={<Canteens />} /> */}
+          {/* <Route path="/Qrcode" element={<Qr />} /> */}
+          {/* <Route path="/shop" element={<Shop />} /> */}
+        </Routes>
+    </>
   );
 }
-
 export default App;
