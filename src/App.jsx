2d8cb8cfd789cd81b1c90919a81943ccb7f4dd56
Rantod-cmd <128260878+Rantod-cmd@users.noreply.github.com>
2025-06-14 18:04:20 +0700

    Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,21 @@
-import { useEffect, useState } from 'react'
-import { supabase } from '../utils/supabase'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Login from "./pages/login";
 
-// function App() {
-//   const [count, setCount] = useState(0)
+export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
 
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-function App() {
-  const [instruments, setInstruments] = useState([]);
-  useEffect(() => {
-    getInstruments();
-  }, []);
-  async function getInstruments() {
-    const { data } = await supabase.from("instruments").select();
-    setInstruments(data);
-  }
   return (
-    <ul>
-      {instruments.map((instrument) => (
-        <li key={instrument.name}>{instrument.name}</li>
-      ))}
-    </ul>
+    <div className="h-screen w-screen bg-[#FBF6E9] flex flex-col overflow-hidden">
+      <div className="bg-green-700 h-[6vh] w-full"></div>
+      <div className="bg-[#74c09b] px-12 py-6 flex items-center justify-center space-x-6 w-full">
+        <div className="bg-green-700 w-16 h-16 rounded-full flex items-center justify-center">
+          <span className="text-white text-3xl">🍴</span>
+        </div>
+        <h1 className="text-5xl font-bold text-gray-800 ">KUMeal</h1>
+      </div>
+      <div className="flex-1 flex items-center justify-center w-full">
+        <Login onSuccess={() => setLoggedIn(true)} />
+      </div>
+    </div>
   );
 }
-export default App;
