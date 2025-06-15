98cc62acab76017932ea4e04a333b7c82486b770
Patiph4t <134424515+mou2548@users.noreply.github.com>
2025-06-15 11:06:03 +0700

    Push src/contexts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react'
-import { supabase } from '../utils/supabase'
 import './App.css'
 
 import { Routes, Route } from 'react-router-dom'
@@ -11,22 +9,25 @@ import QueueScreen from './pages/Queue'
 import Shops from './pages/Shops'
 import PayScreen from './pages/PayScreen'
 import Cart from './pages/Cart'
+import CartIcon from './pages/CartIcon';
+import { CartProvider } from './contexts/CartContext';
 
 
 function App() {
   return (
-    <>
+    <CartProvider>
+      <CartIcon /> 
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/menu/:shopId" element={<Menu />} />
-        <Route path="/payscreen" element={<PayScreen />} />
+        <Route path="/payment/:shopId" element={<PayScreen />} />
         <Route path="/queue/:queueNumber" element={<QueueScreen />} />
         <Route path="/canteens" element={<Canteens />} />
         <Route path="/Qrcode" element={<Qr />} />
         <Route path="/shops/:canteenId" element={<Shops />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
-    </>
+    </CartProvider>
   );
 }
 export default App;
