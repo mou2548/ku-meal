a573624c90c20033f5e6e41b1fc2a4c396195c23
Natrius <natephutare@gmail.com>
2025-06-15 13:10:02 +0700

    Fixing minor details

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import GreenBar from '../components/GreenBar';
 import { supabase } from '../../utils/supabase';
 import { useCart } from '../contexts/CartContext';
+import { Search } from 'lucide-react';
+
 
 const Menu = () => {
   const { shopId } = useParams();
@@ -60,10 +62,11 @@ const Menu = () => {
       
 
       <div className="mt-4 flex items-center bg-white rounded-full shadow px-4 py-2">
+        <Search className="w-5 h-5 text-gray-600 mr-3" />
         <input
           type="text"
           className="flex-grow outline-none text-gray-700 placeholder-gray-400"
-          placeholder="ค้นหาเมนู"
+          placeholder="ค้นหาเมนู..."
           value={search}
           onChange={e => setSearch(e.target.value)}
         />
